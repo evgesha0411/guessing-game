76889e887ef18e5ffc7f6aa6cc07b8dcae1d0a07
evgesha0411 <zhdanovichevgenia@gamil.com>
2017-09-23 15:18:07 +0300

    guessing-game/binary algorithm was implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+	
+    constructor(){}
+		
+    setRange(min, max) 
+	{
+		this.min = min;
+		this.max = max;
     }
 
-    guess() {
-
+    guess()
+	{
+		if((this.max-this.min) % 2 == 0)
+		{
+			this.middle = this.min + ((this.max-this.min)/2);
+		}
+		
+		else
+		{
+			this.middle =  this.min + ((this.max-this.min)/2 + 0.5);
+		}
+		return this.middle;
     }
 
-    lower() {
-
+    lower() 
+	{
+		this.max = this.middle;
     }
 
-    greater() {
-
+    greater() 
+	{
+		this.min = this.middle;
     }
 }
 
